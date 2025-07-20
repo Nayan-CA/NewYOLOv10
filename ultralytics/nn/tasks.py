09aa201fb3559247340ff3df1e6e717e29329d98
Nayan-CA <97055057+Nayan-CA@users.noreply.github.com>
2025-07-20 12:57:25 +0530

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -55,7 +55,8 @@ from ultralytics.nn.modules import (
     RepVGGDW,
     v10Detect,
     DPAM,
-    DALSM
+    DALSM,
+    DWSConv
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
