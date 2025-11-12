b9ca4493329e74ac673099260c23b968121a122c
Nayan-CA <97055057+Nayan-CA@users.noreply.github.com>
2025-11-12 10:48:29 +0530

    Added CBAM

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -56,7 +56,8 @@ from ultralytics.nn.modules import (
     v10Detect,
     DPAM,
     DALSM,
-    DWSConv
+    DWSConv,
+    CBAM
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
