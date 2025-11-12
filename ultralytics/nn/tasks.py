b40a3d07c2c1fa232a00b456aa2e145331689f8a
Nayan-CA <97055057+Nayan-CA@users.noreply.github.com>
2025-11-12 11:35:39 +0530

    Added CBAM

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -894,7 +894,8 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             SCDown,
             C2fCIB,
             DPAM,
-            DALSM
+            DALSM,
+            CBAM
         }:
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
